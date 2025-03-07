e3b83ad6132f9536782c85ddad98e8cadf91e29a
anetafabijanska <anetafabijanska@gmail.com>
2025-03-07 21:04:29 +0000

    changes 2

--- .github/.devcontainer/comments.js
+++ .github/.devcontainer/comments.js
@@ -1,1 +1,28 @@
-// Create web server and listen for requests 
+// Create web server and listen to port 8080
+// This file is used to show comments in the webview
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  // Read the comments.html file
+  fs.readFile('comments.html', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write('File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
+
+// Run this script using the following command
+// node comments.js
+
+// comments.html
