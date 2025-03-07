2dddaf815ccc2077f3846254cf8ce53abddea3a5
anetafabijanska <anetafabijanska@gmail.com>
2025-03-07 20:57:21 +0000

    second commit

---
+++ .github/.devcontainer/member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+  return [
+    {
+      name: 'Web Development',
+      level: '90%'
+    },
+    {
+      name: 'Mobile Development',
+      level: '85%'
+    },
+    {
+      name: 'UI/UX Design',
+      level: '80%'
+    },
+    {
+      name: 'SEO',
+      level: '70%'
+    },
+    {
+      name: 'Marketing',
+      level: '75%'
+    },
+    {
+      name: 'Business Development',
+      level: '65%'
+    }
+  ];
+}
